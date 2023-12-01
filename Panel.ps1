3c965b0dfd60c638123c962ea1e05ad1040a7621
Henning Cullin <henningcullin@gmail.com>
2023-12-01 22:15:57 +0100

    feat: toggle autostart items

--- Panel.ps1
+++ Panel.ps1
@@ -3,8 +3,6 @@ Add-Type -AssemblyName System.Windows.Forms
 $form = [System.Windows.Forms.Form]::new()
 $form.Text = "Windows Panel"
 $form.Size = [System.Drawing.Size]::new(1024, 640)
-$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
-$form.MaximizeBox = $false
 function AutoStart {
 
     $regKeyList = New-Object PSObject -Property @{
@@ -26,7 +24,9 @@ function AutoStart {
     function Format-Path {
         param ([String]$path)
         $path = $path -replace '(\s-.*$)'
-        return $path -replace '"', ''
+        $path = $path -replace '"', ''
+        $extIndex = $Path.LastIndexOf('.')
+        return $Path.Substring(0, $extIndex+4)
     }
 
     function Get-RegItems {
@@ -34,42 +34,35 @@ function AutoStart {
         $items = @()
 
         $DataKey = Get-Item -Path ($regKeyList.$D)
-        $StatusKey = Get-Item -Path ($regKeyList.$S)
+        $StateKey = Get-Item -Path ($regKeyList.$S)
 
         foreach ($value in $Datakey.GetValueNames()) {
             $path = $DataKey.GetValue($value, $null)
-            $vtype = $DataKey.GetValueKind($value)
-            $bin = $StatusKey.GetValue($value, $null)
+            $bin = $StateKey.GetValue($value, $null)
 
             if ($null -ne $bin) {
                 $data = [System.BitConverter]::ToString($bin) -replace '-'
-
                 $start = $data.Substring(0, 2)
-
+                $stateType = [Int]$start
                 $end = $data.Substring(2 + 6)
-
-                $status = switch ([Int]$start) {
+                $state = switch ([Int]$start) {
                     2 { $true }
+                    0 { $true }
                     6 { $true } # Add something to handle different permission levels
+                    1 { $false }
                     3 { $false }
                     default { $null }
                 }
 
-                if ($null -eq $status) {
+                if ($null -eq $state) {
                     continue
                 }
-                if ($status -eq $false -or $end -ge 1) {
 
+                if ($state -eq $false -or $end -ge 1) {
                     $hex = $start + $end
-
-                    # Convert hex to bytes
-                    $bytes = [byte[]]($hex -split '(..)' | Where-Object { $_ } | ForEach-Object { [Convert]::ToByte($_, 16) })
-
-                    # Convert the byte array to a long (Int64)
-                    $filetime = [BitConverter]::ToInt64($bytes, 1) # start from the second byte
-
-                    # Convert the FILETIME (represented as a long) to a DateTime object
-                    $time = [DateTime]::FromFileTimeUtc($filetime)
+                    $bytes = [byte[]]($hex -split '(..)' | Where-Object { $_ } | ForEach-Object { [Convert]::ToByte($_, 16) })# Convert hex to bytes
+                    $filetime = [BitConverter]::ToInt64($bytes, 1) # Convert the byte array to a long (Int64), start from the second byte
+                    $time = [DateTime]::FromFileTimeUtc($filetime)# Convert the FILETIME (represented as a long) to a DateTime object
                 }
                 else {
                     $time = $null
@@ -78,12 +71,11 @@ function AutoStart {
 
             $object = New-Object PSObject -Property @{
                 Name = $value
-                ItemType = 'reg'
-                ValueType = $vtype
                 DataKey = $D
-                StatusKey = $S
+                StateKey = $S
                 Path = $path
-                Status = $status
+                State = $state
+                StateType = $stateType
                 Time = $time
             }
 
@@ -92,8 +84,36 @@ function AutoStart {
         return $items
     }
 
-    function Toggle-Item {
-        
+    function global:Toggle-Item {
+        param ([String]$key, [String]$name, [String]$state)
+
+        $time = Get-Date
+        $filetime = $time.ToFileTimeUtc()
+        $arr = $null
+        switch ($state) {
+            '0' {
+                $arr = [byte[]]@('0', '0', '0', '0') + [System.BitConverter]::GetBytes($filetime)
+            }
+            '2' {
+                $arr = [byte[]]@('2', '0', '0', '0') + [System.BitConverter]::GetBytes($filetime)
+            }
+            '6' {
+                $arr = [byte[]]@('6', '0', '0', '0') + [System.BitConverter]::GetBytes($filetime)
+            }
+            '1' {
+                $arr = [byte[]]@('1', '0', '0', '0') + [System.BitConverter]::GetBytes($filetime)
+            }
+            '3' {
+                $arr = [byte[]]@('3', '0', '0', '0') + [System.BitConverter]::GetBytes($filetime)
+            }
+        }
+        if($null -eq $arr) {
+            Write-Host "ERROR SETTING $name"
+            return
+        }
+
+        Set-ItemProperty -Path $regKeyList.$key -Name $name -Type Binary -Value $arr
+
     }
     function Create-Item {
         if ($yourPath -match '%[a-zA-Z0-9_]+%') {
@@ -104,7 +124,9 @@ function AutoStart {
             # If it doesn't contain environment variables, use REG_SZ
             #New-ItemProperty -Path "HKCU:\SOFTWARE\Example" -Name "MyValue" -PropertyType String -Value $yourPath
         }
+        Write-Host "Create-Item called"
     }
+
     function Draw-Item {
         param ([Object]$item)
         function Get-Icon {
@@ -127,39 +149,43 @@ function AutoStart {
 
         $pnl = [System.Windows.Forms.Panel]::new()
         $pnl.Height = 128
-        $pnl.Width = 245
+        $pnl.Width = 190
         $pnl.Name = 'pnl_'+$item.Name
 
         $lblName = [System.Windows.Forms.Label]::new()
         $lblName.Font = New-Object System.Drawing.Font("Lucida Console", 11, [System.Drawing.FontStyle]::Regular)
-        $lblName.Width = 200
+        $lblName.Width = 190
         $lblName.Height = 32
         $lblName.Name = 'lbl_Name_'+$item.Name
+        $lblName.TextAlign = 'TopCenter'
         $lblName.Text = Get-Name $item.Path
-        
-        $icon = Get-Icon $item.Path
 
+        $icon = Get-Icon $item.Path
         $pbx = [System.Windows.Forms.PictureBox]::new()
         $pbx.Width = 32  # Adjust as needed
         $pbx.Height = 32  # Adjust as needed
-        $pbx.Location = [System.Drawing.Point]::new(64, 32)
+        $pbx.Location = [System.Drawing.Point]::new(82, 32)
         $pbx.Name = $item.Name
         $pbx.Image = $icon.ToBitmap()
 
         $cbx = [System.Windows.Forms.Checkbox]::new()
-        $cbx.Location = [System.Drawing.Point]::new(30, 72)
-        $cbx.Name = 'cbx_Status_' + $item.Name
-        $cbx.Text = If ($item.Status) {'ON'} Else {'OFF'}
-        $cbx.Checked = $item.Status
+        $cbx.Location = [System.Drawing.Point]::new(32, 72)
+        $cbx.Name = 'cbx_State_' + $item.Name
+        $cbx.Text = If ($item.State) {'ON'} Else {'OFF'}
+        $cbx.Anchor = 'None'
+        $cbx.Checked = $item.State
+
         $cbx.Add_CheckedChanged({
             switch ($cbx.Checked) {
                 $true {
                     $cbx.Text = 'ON'
                     Write-Host 'SET KEY' $item.Name 'TO ON'
+                    Toggle-Item -key $item.StateKey -name $item.Name -state '2'
                 }
                 $false {
                     $cbx.Text = 'OFF'
                     Write-Host 'SET KEY' $item.Name 'TO OFF'
+                    Toggle-Item -key $item.StateKey -name $item.Name '3'
                 }
             }
         }.GetNewClosure())
@@ -183,16 +209,14 @@ function AutoStart {
     $form.Controls.Add($flp)
 
     $Items += Get-RegItems -D 'MR' -S 'MSR'
-    $items += Get-RegItems -D 'UR' -S 'USR'
+    $Items += Get-RegItems -D 'UR' -S 'USR'
 
     foreach ($Item in $Items) {
         Draw-Item $Item
-        Write-Output $Item
     }
 
     $form.ShowDialog()
 }
 
+
 AutoStart
-
-
